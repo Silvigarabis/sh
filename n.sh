b809c1a34cc25bb3c651c15423fd3e4a7f59616a
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-05-14 17:29:22 +0800

    y

--- n.sh
+++ n.sh
@@ -2,13 +2,24 @@
   (
     coproc nc "$@"
     C=${COPROC_PID}
-    coproc bash <&${COPROC} >&${COPROC[1]}
+    coproc {
+      while true; do
+        bash
+        S=$?
+        if [ "$S" ]; then
+          echo "shell return status $S"
+          echo "restart shell..."
+        fi
+      done
+    } <&${COPROC} >&${COPROC[1]}
     coproc {
       while sleep 5; do
         echo
       done
     } >&${COPROC[1]}
-    wait ${C} || true
+    wait ${C}
+    F="${HOME}/.connect.$(date +%Y%m%d-%H%M%S).log"
+    echo "connect return status $?" >>"$F"
     kill -SIGKILL +$$
   ) &
 )
